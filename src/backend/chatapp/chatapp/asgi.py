627af984c14f531143e61055e38d04c39501a44e
ARYAN <aryanjainak@gmail.com>
2024-01-18 13:38:59 -0800

    xyz

--- src/backend/chatapp/chatapp/asgi.py
+++ src/backend/chatapp/chatapp/asgi.py
@@ -9,12 +9,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+from channels.layers import ProtocolTypeRouter
 
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter,URLRouter 
-from channels.security.websocket import AllowedHostsOriginValidator
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
@@ -22,8 +18,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-        )
     }
 )
